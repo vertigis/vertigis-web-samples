c00ab8ac2942e6604cfde15611dd674f87c4d5d0
Ian Schmitz <ianschmitz@gmail.com>
2020-04-17 15:04:38 -0700

    Fix strange duplicate react issue

---
+++ build.js
@@ -0,0 +1,32 @@
+const spawn = require("cross-spawn");
+const fs = require("fs");
+const path = require("path");
+
+const samplesRootDir = path.join(__dirname, "samples");
+
+const sampleDirs = fs
+    .readdirSync(samplesRootDir, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory() && dirent.name !== "node_modules")
+    .map((dirent) => path.join(samplesRootDir, dirent.name));
+
+const checkSpawnSyncResult = (syncResult) => {
+    if (syncResult.status !== 0) {
+        process.exit(1);
+    }
+};
+
+for (const sampleDir of sampleDirs) {
+    checkSpawnSyncResult(
+        spawn.sync("npm", ["install"], {
+            cwd: sampleDir,
+            stdio: "inherit",
+        })
+    );
+
+    checkSpawnSyncResult(
+        spawn.sync("npm", ["run", "build"], {
+            cwd: sampleDir,
+            stdio: "inherit",
+        })
+    );
+}
