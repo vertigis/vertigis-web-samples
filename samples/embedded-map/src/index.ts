b9b175031c3f9d37d98abdcd84fe4edf34fb63f0
Ian Schmitz <ianschmitz@gmail.com>
2020-06-26 14:48:53 -0700

    Fix lint error

--- samples/embedded-map/src/index.ts
+++ samples/embedded-map/src/index.ts
@@ -1,7 +1,7 @@
 import { LibraryRegistry } from "@vertigis/web/config";
 import EmbeddedMap, { EmbeddedMapModel } from "./components/EmbeddedMap";
 
-export default function (registry: LibraryRegistry) {
+export default function (registry: LibraryRegistry): void {
     registry.registerComponent({
         name: "embedded-map",
         namespace: "your.custom.namespace",
